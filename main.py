d20f6010d38001e15ad379f24d7ca104223eb357
Anthonyvijay10 <127485990+Anthonyvijay10@users.noreply.github.com>
2023-12-30 11:47:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,18 +24,4 @@ def predict_survival(passenger: Passenger):
     data = passenger.dict()
     sex = data['Sex']
     age = data['Age']
-    
-    if (sex == 'female') or (sex == 'Female'):
-        # If passenger is female, predict "survived"
-        prediction = True
-    elif (sex == 'male') or (sex == 'Male'):
-        if age < 18:
-            # If passenger is male and less than 18 years old, predict "survived"
-            prediction = True
-        else:
-            prediction = False
-    else:
-        # If sex is not specified correctly, return an error response
-        raise HTTPException(status_code=400, detail="Invalid input")
-
     return {"survived": prediction}
